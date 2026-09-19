d026882f5094f4d2bd421dcb33d320f39cd373f1
sejalchhapre02122003 <sejalchhapre123@gmail.com>
2026-09-19 11:25:07 +0530

    fix

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -24,13 +24,13 @@ export default defineConfig({
     host: true,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'http://127.0.0.1:5000',
         changeOrigin: true,
       },
       '/uploads': {
-        target: 'http://localhost:5000',
+        target: 'http://127.0.0.1:5000',
         changeOrigin: true,
       },
     },
   },
-});																																																																																																																																																																																																																																	
+});
